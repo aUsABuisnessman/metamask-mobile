eb4fffe2aa69698857499a585f6bb8c86612b817
Owen Craston <owengc12@gmail.com>
2024-11-15 15:57:16 -0800

    close after creating bitcoin account

--- app/components/Views/AddAccountActions/AddAccountActions.tsx
+++ app/components/Views/AddAccountActions/AddAccountActions.tsx
@@ -73,7 +73,8 @@ const AddAccountActions = ({ onBack }: AddAccountActionsProps) => {
       // eslint-disable-next-line no-console
       console.log('Bitcoin account creation failed', error);
     } finally {
-      setIsLoading(true);
+      onBack();
+      setIsLoading(false);
     }
   };
   ///: END:ONLY_INCLUDE_IF
